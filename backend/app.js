0e9952eead7293ec4875e85dce7caa6dbf333825
AdrienBriaux <devaccess@hotmail.com>
2022-03-30 14:25:48 +0200

    creation controllers

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const Thing = require('./models/thing');
 const app = express();
 const mongoose = require('mongoose');
+const stuffRoutes = require('./routes/stuff');
 
 // Connection à la data base
 
 mongoose.connect('mongodb+srv://Ibanez:Ibanez@cluster0.fpqz2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
@@ -16,13 +19,17 @@ mongoose.connect('mongodb+srv://Ibanez:Ibanez@cluster0.fpqz2.mongodb.net/myFirst
 
 app.use(express.json());
 
+// Chemin routes vers stuff
+
+app.use('/api/stuff', stuffRoutes);
+
 // CORS
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 module.exports = app;
